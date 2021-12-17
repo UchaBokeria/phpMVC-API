a7704e3ea847087753af9cd03cb95573ecce8512
uchaia ucha <uch1bokeria@gmail.com>
2021-12-17 12:35:38 +0400

    x

--- API/Products/Controller.php
+++ API/Products/Controller.php
@@ -2,24 +2,50 @@
 
     class Products extends Database
     {
+        public function __construct()
+        {
+            parent::__construct();
+
+            // here gos interface
+        }
+
         public function Create()
         {
+            parent::SET("    INSERT INTO help SET question = :question, answer = :answer , lang_id = :lang_id ;", 
+                                    [ "question" => $_POST["question"] ,
+                                      "answer"   => $_POST["answer"] ,
+                                      "lang_id"  => $_POST["language"] 
+                                    ]);
 
+            return (parent::GetLastId() > 0)  ? 
+                    ['error' => false, 'success' => true, 'message' =>  ' Record ' . parent::GetLastId() . ' has been created; '] : 
+                    ['error' => false, 'success' => false, 'message' => ' Record ' . parent::GetLastId() . ' has NOT been created; '] ;
         }
 
         public function Read()
         {
-
+            $activeds = "";
+            if($_POST["onlyActiveds"]) $activeds = " AND actived = 1 ";
+            return parent::GET(" SELECT id, question, answer, lang_id FROM help WHERE lang_id = :lang $activeds ;", ["lang" => $_POST["language"]]);
         }
 
         public function Update()
         {
-
+            return (parent::SET(" UPDATE help SET question = :question, answer = :answer  WHERE id = :id;", 
+                                [   "question" => $_POST["question"], 
+                                    "answer" => $_POST["answer"],
+                                    "id" => $_POST["id"]
+                                ] ) > 0 ) ? 
+                    ['error' => false, 'success' => true, 'message' => $_POST["id"]. ' Record has been deleted; '] : 
+                    ['error' => false, 'success' => false, 'message' => ' Record has NOT been found; '] ;;
         }
 
         public function Delete()
         {
-
+            $result = parent::SET(" UPDATE help SET  actived = 0  WHERE id IN(:list) ;", ["list" => explode(',', $_POST["list"]) ]);
+            return ($result > 0)  ? 
+                    ['error' => false, 'success' => true, 'message' => $result . ' Records has been deleted; '] : 
+                    ['error' => false, 'success' => false, 'message' => ' Records has NOT been found; '] ;
         }
 
     }
--- API/Translate/Controller.php
+++ API/Translate/Controller.php
@@ -2,24 +2,52 @@
 
     class Translate extends Database
     {
+        public function __construct()
+        {
+            parent::__construct();
+
+            // here gos interface
+        }
+
         public function Create()
         {
+            parent::SET("    INSERT INTO `translate` SET lang_id = :lang_id, lang_key: :lang_key , page_id = :page_id , lang_value = :lang_value ;", 
+                                    [ "lang_id" => $_POST["language"] ,
+                                      "page_id"   => $_POST["page_id"] ,
+                                      "lang_key"   => $_POST["lang_key"] ,
+                                      "lang_value"  => $_POST["lang_value"] 
+                                    ]);
 
+            return (parent::GetLastId() > 0)  ? 
+                    ['error' => false, 'success' => true, 'message' =>  ' Record ' . parent::GetLastId() . ' has been created; '] : 
+                    ['error' => false, 'success' => false, 'message' => ' Record ' . parent::GetLastId() . ' has NOT been created; '] ;
         }
 
         public function Read()
         {
-
+            $activeds = "";
+            if($_POST["onlyActiveds"]) $activeds = " AND actived = 1 ";
+            return parent::GET(" SELECT id, page_id , lang_id, lang_key, lang_value 
+                                 FROM `translate` WHERE lang_id = :lang AND page_id = :page_id $activeds ;", 
+                                 ["lang" => $_POST["language"], "page_id" => $_POST["page_id"]]);
         }
 
         public function Update()
         {
-
+            return (parent::SET(" UPDATE `translate` SET lang_value = :lang_value  WHERE id = :id;", 
+                                [   "lang_value" => $_POST["lang_value"], 
+                                    "id" => $_POST["id"]
+                                ] ) > 0 ) ? 
+                    ['error' => false, 'success' => true, 'message' => $_POST["id"]. ' Record has been deleted; '] : 
+                    ['error' => false, 'success' => false, 'message' => ' Record has NOT been found; '] ;;
         }
 
         public function Delete()
         {
-
+            $result = parent::SET(" UPDATE `translate` SET  actived = 0  WHERE id IN(:list) ;", ["list" => explode(',', $_POST["list"]) ]);
+            return ($result > 0)  ? 
+                    ['error' => false, 'success' => true, 'message' => $result . ' Records has been deleted; '] : 
+                    ['error' => false, 'success' => false, 'message' => ' Records has NOT been found; '] ;
         }
 
     }
