ba50cb1160fcecbb51bd989efd58132609b334a1
UchaBokeria <ucha1bokeria@gmail.com>
2021-12-19 04:16:56 -0800

    x

--- API/Products/Controller.php
+++ API/Products/Controller.php
@@ -11,68 +11,121 @@
 
         public function Create()
         {
-            parent::SET("    INSERT INTO help SET question = :question, answer = :answer , lang_id = :lang_id ;", 
-                                    [ "question" => $_POST["question"] ,
-                                      "answer"   => $_POST["answer"] ,
-                                      "lang_id"  => $_POST["language"] 
-                                    ]);
+            parent::SET("   INSERT INTO posts SET 
+                                `user_id`     = :user_id, 
+                                `title`       = :title ,
+                                `dir`         = :dir ,
+                                `country_id`  = :country ,
+                                `city_id`     = :city ,
+                                `created`     = NOW() , 
+                                `description` = :description ;", 
 
-            return (parent::GetLastId() > 0)  ? 
-                    ['error' => false, 'success' => true, 'message' =>  ' Record ' . parent::GetLastId() . ' has been created; '] : 
-                    ['error' => false, 'success' => false, 'message' => ' Record ' . parent::GetLastId() . ' has NOT been created; '] ;
+                        [   
+                            "user_id"     => $_POST["user_id"] ,
+                            "title"       => $_POST["title"] ,
+                            "dir"         => $_POST["thumbnail"] ,
+                            "country"     => $_POST["country"] ,
+                            "city"        => $_POST["city"] ,
+                            "description" => $_POST["description"] 
+                        ]);
+            $id = parent::GetLastId();
+
+            foreach (json_decode($_POST["details"]) as $key => $value) {
+                
+                parent::SET("   INSERT INTO `post_details` set 
+                                            `post_id` = :post_id,
+                                            `user_id` = :user_id,
+                                            `value`   = :value,
+                                            `key`     = :title ; ",
+                            [
+                                'post_id' => $id,
+                                'value'   => $value,
+                                'title'   => $key,
+                                'user_id' => $_POST["user_id"]
+                            ]);
+
+            }
+
+
+            return ($id > 0)  ? 
+                    ['error' => false, 'success' => true, 'message' =>  ' Record ' . $id . ' has been created; '] : 
+                    ['error' => false, 'success' => false, 'message' => ' Record ' . $id . ' has NOT been created; '] ;
         }
 
         public function Read()
         {
             $activeds = "";
             if($_POST["onlyActiveds"]) $activeds = " AND actived = 1 ";
+
             return parent::GET("    SELECT  posts.id,
                                             posts.created,
                                             posts.user_id,
                                             posts.title,
                                             posts.description,
-                                            post_media.dir,
+                                            posts.dir AS thumbnail,
                                             CONCAT( '{', 
                                                 GROUP_CONCAT( 
-                                                    post_settings.title, ':' ,post_details.`value`
+                                                    post_details.`key`, ':' , post_details.`value`
                                                 ) , '}'
                                             ) AS details
 
                                     FROM posts 
                                     LEFT JOIN post_details ON post_details.post_id = posts.id
-                                    LEFT JOIN post_settings ON post_settings.id = post_details.post_setting_id
                                     LEFT JOIN post_media ON post_media.id = posts.thumbnail_id
                                     LEFT JOIN vips ON vips.id = posts.vip_status_id
                                     WHERE post_details.actived = 1 $activeds
-                                    GROUP BY posts.id
-            ");
-        }
-
-        public function Details()
-        {
-            $activeds = "";
-            if($_POST["onlyActiveds"]) $activeds = " AND actived = 1 ";
-            return parent::GET(" SELECT id, question, answer, lang_id FROM help WHERE lang_id = :lang $activeds ;", ["lang" => $_POST["language"]]);
+                                    GROUP BY posts.id ; ");
         }
 
         public function Update()
         {
-            return (parent::SET(" UPDATE help SET question = :question, answer = :answer  WHERE id = :id;", 
-                                [   "question" => $_POST["question"], 
-                                    "answer" => $_POST["answer"],
-                                    "id" => $_POST["id"]
-                                ] ) > 0 ) ? 
-                    ['error' => false, 'success' => true, 'message' => $_POST["id"]. ' Record has been deleted; '] : 
-                    ['error' => false, 'success' => false, 'message' => ' Record has NOT been found; '] ;;
+            $count = 0;
+            $count +=   parent::SET("UPDATE `posts` SET 
+                                            `user_id`     = :user_id, 
+                                            `title`       = :title ,
+                                            `dir`         = :dir ,
+                                            `country_id`  = :country ,
+                                            `city_id`     = :city ,
+                                            `created`     = NOW() , 
+                                            `description` = :description 
+                                    WHERE   `id`          = :post_id;", 
+
+                                [   "user_id"     => $_POST["user_id"] ,
+                                    "title"       => $_POST["title"] ,
+                                    "dir"         => $_POST["thumbnail"] ,
+                                    "country_id"  => $_POST["country"] ,
+                                    "city_id"     => $_POST["city"] ,
+                                    "description" => $_POST["description"],
+                                    "post_id"     => $_POST["post_id"]
+                                ]);
+
+            foreach (json_decode($_POST["details"]) as $key => $value) {
+
+                $count +=   parent::SET("   UPDATE  `post_details` set 
+                                                    `user_id` = :user_id,
+                                                    `value`   = :value,
+                                                    `key`     = :title 
+                                            WHERE   `post_id` = :post_id; ",
+                                    [
+                                        'post_id' => $_POST["post_id"],
+                                        'value'   => $value,
+                                        'title'   => $key,
+                                        'user_id' => $_POST["user_id"]
+                                    ]);
+
+            }
+
+            return ( $count != 0 ) ? 
+                    ['error' => false, 'success' => true, 'message' => $_POST["post_id"]. ' Record has been updated; '] : 
+                    ['error' => false, 'success' => false, 'message' => ' Nothing has been updated; '] ;
         }
 
         public function Delete()
         {
-            $result = parent::SET(" UPDATE help SET  actived = 0  WHERE id IN(:list) ;", ["list" => explode(',', $_POST["list"]) ]);
+            $result = parent::SET(" UPDATE posts SET  actived = 0  WHERE id IN(:list) ;", ["list" => explode(',', $_POST["list"]) ]);
             return ($result > 0)  ? 
                     ['error' => false, 'success' => true, 'message' => $result . ' Records has been deleted; '] : 
                     ['error' => false, 'success' => false, 'message' => ' Records has NOT been found; '] ;
         }
 
     }
-    
