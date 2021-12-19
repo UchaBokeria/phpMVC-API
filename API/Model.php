7b7798c31fea364e03029a9010a55ee1ec36d0c1
UchaBokeria <ucha1bokeria@gmail.com>
2021-12-19 04:29:37 -0800

    g

--- API/Model.php
+++ API/Model.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-    include_once "../Routes/Configure.php";
+    include_once "../../Routes/Configure.php";
 
     new Configure("RouteMap");
     new Configure("Database");
